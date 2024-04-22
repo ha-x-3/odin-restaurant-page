b9919d5ed7c6e1b88175a23c1ad91aa14bcf4340
Brittney Epperson <beppers87@hotmail.com>
2024-04-22 16:31:25 -0500

    Added styling, background, and custom font

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,11 @@
 const createMenuPage = () => {
-    alert("Menu Working!")
+    const content = document.querySelector("#content");
+    const pageContent = document.createElement('div');
+    pageContent.classList.add("page-content");
+
+    
+
+    content.appendChild(pageContent);
 }
 
 export default createMenuPage;
