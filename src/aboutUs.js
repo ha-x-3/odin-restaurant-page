6d0917a12ed047e185d3794af7e05794a6051bbe
Brittney Epperson <beppers87@hotmail.com>
2024-04-22 17:18:05 -0500

    Added custom bold font

--- src/aboutUs.js
+++ src/aboutUs.js
@@ -10,11 +10,30 @@ const createAboutUsPage = () => {
 
     //Create and append hours
     const hours = document.createElement('ul');
-    hours.textContent = "Hours:";
-    const monday = document.createElement("li");
-    monday.textContent = "Mon: 10am - 8pm";
-    hours.appendChild(monday);
-
+    hours.textContent = "Hours";
+    hours.id = "hours-list";
+    const sunday = document.createElement('li');
+	sunday.textContent = 'Sun: 10am - 6pm';
+	hours.appendChild(sunday);
+    const monday = document.createElement('li');
+	monday.textContent = 'Mon: 10am - 8pm';
+	hours.appendChild(monday);
+    const tuesday = document.createElement("li");
+    tuesday.textContent = "Tues: 10am - 8pm";
+    hours.appendChild(tuesday);
+    const wednesday = document.createElement('li');
+	wednesday.textContent = 'Wed: 10am - 8pm';
+	hours.appendChild(wednesday);
+    const thursday = document.createElement('li');
+	thursday.textContent = 'Thurs: 10am - 8pm';
+	hours.appendChild(thursday);
+    const friday = document.createElement('li');
+	friday.textContent = 'Fri: 10am - 10pm';
+	hours.appendChild(friday);
+    const saturday = document.createElement('li');
+	saturday.textContent = 'Sat: 10am - 10pm';
+	hours.appendChild(saturday);
+    pageContent.appendChild(hours);
 
     content.appendChild(pageContent);
 }
